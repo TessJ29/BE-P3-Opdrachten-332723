605cdb0c46ef7667ad4297c8013c8ca856cd20d4
Tess Jansen <tess.jansen29@gmail.com>
2023-01-28 19:36:51 +0100

    oprdacht 11 backend

---
+++ app/views/landingpages/index.php
@@ -0,0 +1,3 @@
+<p3><?php $data['title']; ?></p3>;
+<a href="<?= URLROOT . '/Lessen/index'?>">Overzicht rijlessen</a>
+<a href="<?= URLROOT . '/Instructeurs/index'?>">Overzicht instructeurs in dienst</a>
